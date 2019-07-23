7f1379006979ddc9a6d09ba5492e7e645a621021
Dong-Won Lee <48680224+Lee-DongWon@users.noreply.github.com>
2019-07-23 20:49:51 +0900

    Add files via upload

---
+++ 1016.cpp
@@ -0,0 +1,36 @@
+#include <stdio.h>
+long long numbers[1000001];
+long long prinum[10000];
+
+int main(void) {
+
+	long long max, min;
+
+	int count = 0;
+
+	scanf("%lld %lld", &min, &max);
+
+
+
+	for (long long i = 2; i*i <= max; i++) {
+
+
+		long long x = min / (i*i);
+		if (min % (i*i) != 0)
+			x++;
+
+		while (x*(i*i) <= max) {
+			numbers[x*(i*i) - min] = 1;
+			x++;
+		}
+
+	}
+	for (int i = 0; i <= max - min; i++) {
+		if (numbers[i] == 0)
+			count++;
+	}
+
+
+	printf("%d\n", count);
+	return 0;
+}
